95c17f7b5c9ceb322867ffbe7514eb4c656afaa1
Joao <adm@noarcloud.com>
2021-01-21 21:50:09 -0300

    Att

--- src/main/java/br/com/imobzi/batch/config/WebSocketConfig.java
+++ src/main/java/br/com/imobzi/batch/config/WebSocketConfig.java
@@ -2,19 +2,23 @@ package br.com.imobzi.batch.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	
+	@CrossOrigin
 	@Override
 	  public void configureMessageBroker(MessageBrokerRegistry config) {
 	    config.enableSimpleBroker("/topic");
 	  }
 
+	@CrossOrigin
 	  @Override
 	  public void registerStompEndpoints(StompEndpointRegistry registry) {
 	    registry.addEndpoint("/immobile-websocket").withSockJS();
